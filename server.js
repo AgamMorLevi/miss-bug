afea9be31e9b820439eb84ba2b684766243d1e12
AgamMorLevi <agammor96@gmail.com>
2025-04-23 01:53:41 +0300

    CRUDL done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 
 import { bugService } from './services/bug.service.js'
-import { loggerService } from './services/Logger.service.js'
+import { loggerService } from './services/logger.service.js' 
 
 const app = express() 
 
@@ -13,6 +13,7 @@ app.use(cookieParser())
 //Provide an API for Bugs CRUDL: Implement one by one along with a bugService
 
 //TODO: List of bugs
+//* Read
 app.get('/api/bug', (req, res) => {
    bugService.query()
    .then(bugs => res.send(bugs))
@@ -24,12 +25,48 @@ app.get('/api/bug', (req, res) => {
 }) 
 
 //TODO: Set Defult value
-app.get('/api/bug/save', (req, res) => {}) 
+//* Create/Edit 
+app.get('/api/bug/save', (req, res) => {
+
+    const bugToSave ={
+       _id: req.query._id|| '',
+      title: req.query.title || '',
+      description: req.query.description || '',
+      severity: +req.query.severity || 0,
+    }
+
+    bugService.save(bugToSave)
+    .then(savedBug => res.send(savedBug))
+        .catch(err => {
+            loggerService.error('Cannot save bug', err)
+            res.status(500).send('Cannot save bug')
+        })
+
+}) 
 
 //TODO:erfactor Err
-app.get('/api/bug/:bugId', (req, res) => {}) 
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot load bug')
+        })
 
-app.get('/api/bug/:bugId/remove', (req, res) => {}) 
+}) 
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send('bug Removed'))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug')
+        })
+
+
+}) 
 
 //TODO:Make the option for PDF
 
--- services/util.service.js
+++ services/util.service.js
@@ -6,7 +6,10 @@ export const utilService = {
   readJsonFile,
   download,
   httpGet,
-  makeId
+  makeId,
+  getRandomIntInclusive,
+  makeLorem
+
 }
 
 
@@ -61,4 +64,20 @@ function makeId(length = 5) {
       text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
   return text
+}
+
+function makeLorem(size = 100) {
+  var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
+  var txt = ''
+  while (size > 0) {
+      size--
+      txt += words[Math.floor(Math.random() * words.length)] + ' '
+  }
+  return txt
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
